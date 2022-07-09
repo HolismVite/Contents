79cd0f7b997edb597fb81ccbaeb9759220634cf1
Saeed Nemati <sn.1361@gmail.com>
2022-07-09 18:09:11 +0430

    committed by script

--- Admin/Section/ConfigureItems.jsx
+++ Admin/Section/ConfigureItems.jsx
@@ -9,8 +9,8 @@ const ConfigureItems = ({ entity }) => {
     const [chosenValues, setChosenValues] = useState([])
 
     const inputs = <Checks
-        itemsUrl={`/entityConfig/getItems?entityType=${entity.relatedItems.entityType}&entityGuid=${entity.guid}`}
-        checkedItemsUrl={`/entityConfig/getValues?entityType=${entity.relatedItems.entityType}&entityGuid=${entity.guid}`}
+        itemsUrl={`/entityConfigItem/list?entityType=${entity.relatedItems.entityType}`}
+        checkedItemsUrl={`/entityConfig/getValues?entityGuid=${entity.guid}`}
         show={item => item.name}
         choose={item => item.tagGuid || item.guid}
         set={setChosenValues}
